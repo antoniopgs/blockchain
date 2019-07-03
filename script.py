d8418a2ba8ba47fdcf826fc3b9da2e56c070e1b9
António Gonçalves <apgoncalves@outlook.pt>
2019-07-03 19:57:32 +0100

    Fix iterator

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
-# ---------- DEPENDENCIES ----------
+# ---------- DEPENDENCIES AND OTHERS ----------
 import datetime
-
+blockchains = []
+n = 0
 
 # ---------- BLOCK CLASS ----------
 class Block:
@@ -43,13 +44,31 @@ class Blockchain:
                     blockchains.append(self)
                         
     def __iter__(self):
-        current_block = self.tail_block
-        return current_block
+        return self
 
     def __next__(self):
-        current_block = current_block.previous_block
-        return current_block
-
+        # If Blockchain is empty:
+        if not self.tail_block:
+            raise StopIteration
+        # If Blockchain is not empty:
+        else:
+            """ 'n' counts what iteration of "__next__()" the program is in.
+                Or in other words, how many times the "__next__()" function has been run."""
+            global n
+            n += 1
+            # If it's the first "__next__()" iteration:
+            if n == 1:
+                return self.tail_block
+            # If it's after the first "__next__()" iteration:
+            else:
+                current_block = self.tail_block
+                for i in range(n-1):
+                    current_block = current_block.previous_block
+                if not current_block:
+                    raise StopIteration
+                else:
+                    return current_block
+            
     def __repr__(self):
         return f"{self.name}"
 
@@ -76,54 +95,17 @@ class Blockchain:
                     current_block = current_block.previous_block
 
 
-# ---------- MAIN CYCLE ----------
-blockchains = []
-while True:
-    # Blockchains main view
-    print("---------- BLOCKCHAINS ----------")
-    # If no Blockchains exist
-    if not blockchains:
-        print("There are no existing Blockchains.")
-    # If Blockchains do exist
-    else:
-        for blockchain in blockchains:
-            print(f"- {blockchain}")
-    user_input_1 = input("""N - New Blockchain
-Q - Quit
+# ---------- TESTS ----------
+# Testing out my iterator with an empty Blockchain
+blockchain_1 = Blockchain()
+for block in blockchain_1:
+    print(block)
 
-Insert Command: """)
-    if any(blockchain.name == user_input_1 for blockchain in blockchains):
-        while True:
-            user_input_2 = input(f"""---------- {blockchain.name.upper()} ----------
-1 - New Block
-2 - View Blocks
-3 - Delete Blockchain
-4 - Back
+blockchain_2 = Blockchain()
+blockchain_2.add_block("António", 200000, "Tim")
+blockchain_2.add_block("António", 100000, "Bob")
+blockchain_2.add_block("António", 50000, "John")
+blockchain_2.add_block("António", 50000, "Bill")
+for block in blockchain_2:
+    print(block)
 
-Insert Command: """)
-            if user_input_2 == "1":
-                print(f"\n----- {blockchain.name.upper()} - NEW BLOCK -----")
-                user_input_3 = input("Insert Your Name: ")
-                user_input_4 = input("Insert Value: ")
-                user_input_5 = input("Insert Recipient: ")
-                blockchain.add_block(user_input_3, user_input_4, user_input_5)
-                print()
-            elif user_input_2 == "2":
-                print(f"\n----- {blockchain.name.upper()} - VIEW BLOCKS -----")
-                blockchain.view()
-            elif user_input_2 == "3":
-                blockchains.remove(blockchain)
-                print(f"{blockchain.name.upper()} SUCCESSFULLY DELETED\n")
-                break
-            elif user_input_2 == "4":
-                break
-            else:
-                print("Invalid Command.\n")
-    # If User selects to create a new Blockchain
-    elif user_input_1 == "N":
-        new_blockchain = Blockchain()
-        print(f"{new_blockchain.name.upper()} SUCCESSFULLY CREATED\n")
-    elif user_input_1 == "Q":
-        break
-    else:
-        print("Invalid Command.\n")
